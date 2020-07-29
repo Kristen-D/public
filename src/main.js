d6827b32f97fbc3c7f12c3cdeaf79949fa2b3c58
kristin <Aa422513479>
2020-07-29 11:59:38 +0800

    初次使用antv

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ant from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(ant)
 
 Vue.config.productionTip = false
 
