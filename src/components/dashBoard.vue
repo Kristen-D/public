89e7a825eaae7deeb70f3506eabb0c4393e421b2
kristin <Aa422513479>
2020-07-29 14:00:58 +0800

    页面布局完成

--- src/components/dashBoard.vue
+++ src/components/dashBoard.vue
@@ -2,43 +2,25 @@
   <a-layout id="components-layout-demo-fixed">
     <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
       <div class="logo" />
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        :default-selected-keys="['2']"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">
-          nav 1
-        </a-menu-item>
-        <a-menu-item key="2">
-          nav 2
-        </a-menu-item>
-        <a-menu-item key="3">
-          nav 3
-        </a-menu-item>
+      <a-menu theme="dark" mode="horizontal":default-selected-keys="['1']":style="{ lineHeight: '64px', float: 'left' }">
+        <a-menu-item key="1">首页</a-menu-item>
+        <a-menu-item key="2">试用</a-menu-item>
+        <a-menu-item key="3">关于</a-menu-item>
       </a-menu>
     </a-layout-header>
-    <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }">
-      <a-breadcrumb :style="{ margin: '16px 0' }">
-        <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
-      </a-breadcrumb>
-      <div :style="{ background: '#fff', padding: '24px', minHeight: '380px' }">
+    <a-layout-content :style="{padding: '0px 50px',marginTop: '64px' }">
+      <div :style="{ background: '#fff', padding: '24px', height: '800px' }">
         Content
       </div>
     </a-layout-content>
-    <a-layout-footer :style="{ textAlign: 'center' }">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
   </a-layout>
 </template>
 <style>
   #components-layout-demo-fixed .logo {
     width: 120px;
     height: 31px;
-    background: rgba(255, 255, 255, 0.2);
+    background: url(/static/images/logo.png) -9% no-repeat;
+    /*background: rgba(255, 255, 255, 0.2);*/
     margin: 16px 24px 16px 0;
     float: left;
   }
